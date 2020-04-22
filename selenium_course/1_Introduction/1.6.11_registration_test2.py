feb1c38fe7c6640f0dcee39642ca18d6f6542478
gsolovev <solovev.grigorii@gmail.com>
2020-04-22 15:19:39 +0200

    selenium

---
+++ selenium_course/1_Introduction/1.6.11_registration_test2.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+
+try:
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    list = ["input[placeholder='Input your first name']", "input[placeholder='Input your last name']", "input[placeholder='Input your email']"]
+    for a in list:
+            input = browser.find_element_by_css_selector(a)
+            input.send_keys("test")
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    time.sleep(1)
+
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    welcome_text = welcome_text_elt.text
+
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    time.sleep(10)
+    browser.quit()
